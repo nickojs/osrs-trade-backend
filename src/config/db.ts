5508b024475ae240018910353024d24e69c45406
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-25 16:49:49 -0300

    feat: create db config

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
+  synchronize: process.env.NODE_ENV === 'production' ? false : true,
+  retryAttempts: 2,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} as TypeOrmModuleOptions;
