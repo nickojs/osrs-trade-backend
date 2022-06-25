fa37f72f0392c28ef73019311a497d4ac3462c8c
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-25 16:51:02 -0300

    feat: config dotenv and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { config } from 'dotenv';
+config();
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/user/user.controller';
+import db from './config/db';
+
+console.log(db);
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(db)],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
