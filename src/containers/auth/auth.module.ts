c0d19b4a6585392532a1f4d6fd6a777bf2a78c9e
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-26 19:03:47 -0300

    feat: implement jwt strategy

--- src/containers/auth/auth.module.ts
+++ src/containers/auth/auth.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../user/user.module';
 import { UserService } from '../user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy, UserService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION,
+      },
+    }),
+  ],
+  providers: [AuthService, UserService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/containers/auth/auth.service.ts
+++ src/containers/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
 import { comparePw } from './helpers';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findUser(username);
@@ -15,5 +20,12 @@ export class AuthService {
       return rest;
     }
     return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.username, id: user.id };
+    return {
+      token: this.jwtService.sign(payload),
+    };
   }
 }
