a32a133b43814fbd6d9d48627f6bede6813b1ee9
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-06-29 01:07:05 -0300

    feat: extend error descriptions

--- src/containers/auth/local.strategy.ts
+++ src/containers/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -10,10 +10,26 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(username, password);
-    if (!user) {
-      throw new UnauthorizedException();
+    try {
+      const user = await this.authService.validateUser(username, password);
+      if (!user) {
+        throw new HttpException(
+          {
+            error: 'Check your credentials',
+            status: HttpStatus.UNAUTHORIZED,
+          },
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        {
+          error: 'Invalid request',
+          status: HttpStatus.BAD_REQUEST,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
     }
-    return user;
   }
 }
