4cbcaaf5cf778ba6a6fd951d254ae93eaf5d5464
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-03 02:38:28 -0300

    refactor: create helper functions and better logic handlers

--- src/trade.gateway.ts
+++ src/trade.gateway.ts
@@ -29,7 +29,9 @@ interface StatusPayload extends ConnectedPayload {
   },
 })
 export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Socket;
   constructor(private dataSource: DataSource) {}
+  private connectedUsers: ConnectedUser[] = [];
 
   private connectedUserFactory(
     userId: string,
@@ -50,8 +52,27 @@ export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
     };
   }
 
-  @WebSocketServer() server: Socket;
-  private connectedUsers: Partial<ConnectedUser>[] = [];
+  private getUser(id: string, identifier: 'socketId' | 'userId') {
+    switch (identifier) {
+      case 'socketId':
+        return {
+          user: this.connectedUsers.find((user) => user.socketId === id),
+          index: this.connectedUsers.findIndex((user) => user.socketId === id),
+        };
+      case 'userId':
+        return {
+          user: this.connectedUsers.find((user) => user.userId === id),
+          index: this.connectedUsers.findIndex((user) => user.userId === id),
+        };
+      default:
+        throw new Error('[getUser] unknown identifier provided');
+    }
+  }
+
+  private cleanUser(user: ConnectedUser) {
+    const { userId, username, socketId } = user;
+    return this.connectedUserFactory(userId, username, socketId);
+  }
 
   async handleConnection(client: any, ...args: any[]) {
     console.log(this.connectedUsers);
@@ -59,29 +80,24 @@ export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('afterConnect')
   setUser(client: Socket, payload: ConnectedPayload) {
-    try {
-      const findExistingUser = this.connectedUsers.find(
-        (user) => user.userId === payload.userId,
-      );
-      if (findExistingUser) return;
+    const user = this.getUser(payload.userId, 'userId');
+    if (user.index > -1) return;
 
-      const createSocketUser = this.connectedUserFactory(
-        payload.userId,
-        payload.username,
-        client.id,
-      );
-      this.connectedUsers.push(createSocketUser);
-      console.log(this.connectedUsers);
-    } catch (error) {
-      client.emit('error', { message: "couldn't update status", error });
-    }
+    const createSocketUser = this.connectedUserFactory(
+      payload.userId,
+      payload.username,
+      client.id,
+    );
+
+    this.connectedUsers.push(createSocketUser);
+    console.log(this.connectedUsers);
   }
 
   async handleDisconnect(client: Socket) {
-    const currentUserIndex = this.connectedUsers.findIndex(
-      (user) => user.socketId === client.id,
-    );
-    this.connectedUsers.splice(currentUserIndex, 1);
+    const user = this.getUser(client.id, 'socketId');
+    if (user.index > -1) {
+      this.connectedUsers.splice(user.index, 1);
+    }
     console.log(this.connectedUsers);
   }
 
@@ -103,33 +119,26 @@ export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @SubscribeMessage('initTrade')
   async initTrade(client: Socket, payload: any) {
     const { targetId } = payload;
-    const targetUser = this.connectedUsers.find(
-      (user) => user.userId === targetId,
-    );
-    if (!targetUser) return client.emit('error', { message: 'User is away' });
+    const targetUser = this.getUser(targetId, 'userId');
+    if (targetUser.index === -1)
+      return client.emit('error', { message: 'User is away' });
 
-    const currentUser = this.connectedUsers.find(
-      (user) => user.socketId === client.id,
-    );
+    const currentUser = this.getUser(client.id, 'socketId');
 
-    const currentUserIndex = this.connectedUsers.findIndex(
-      (user) => user.socketId === client.id,
-    );
-
-    currentUser.acceptTrade = true;
-    currentUser.trading = {
+    currentUser.user.acceptTrade = true;
+    currentUser.user.trading = {
       isTrading: true,
       recipientId: targetId,
     };
 
-    this.connectedUsers.splice(currentUserIndex, 1, currentUser);
+    this.connectedUsers.splice(currentUser.index, 1, currentUser.user);
 
-    client.to(targetUser.socketId).emit('initTrade', {
-      message: `${currentUser.username} wants to trade...`,
+    client.to(targetUser.user.socketId).emit('initTrade', {
+      message: `${currentUser.user.username} wants to trade...`,
       data: {
         user: {
-          userId: currentUser.userId,
-          socketId: currentUser.socketId,
+          userId: currentUser.user.userId,
+          socketId: currentUser.user.socketId,
         },
       },
     });
@@ -139,29 +148,58 @@ export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('declineTrade')
   async declineTrade(client: Socket, payload: any) {
-    const currentUser = this.connectedUsers.find(
-      (user) => user.socketId === client.id,
-    );
+    const currentUser = this.getUser(client.id, 'socketId');
+    const targetUser = this.getUser(payload.tradingId, 'userId');
 
-    const targetUser = this.connectedUsers.find(
-      (user) => user.userId === payload.tradingId,
-    );
-    const targetUserIndex = this.connectedUsers.findIndex(
-      (user) => user.userId === payload.tradingId,
-    );
+    const cleanTargetUser = this.cleanUser(targetUser.user);
+    const cleanCurrentUser = this.cleanUser(currentUser.user);
 
-    targetUser.acceptTrade = false;
-    targetUser.trading = {
-      isTrading: false,
-      recipientId: '',
-    };
+    this.connectedUsers.splice(targetUser.index, 1, cleanTargetUser);
+    this.connectedUsers.splice(currentUser.index, 1, cleanCurrentUser);
 
-    this.connectedUsers.splice(targetUserIndex, 1, targetUser);
-
-    client.to(targetUser.socketId).emit('declineTrade', {
-      message: `${currentUser.username} declined the trade`,
+    client.to(targetUser.user.socketId).emit('declineTrade', {
+      message: `${currentUser.user.username} declined the trade`,
     });
 
+    console.log(this.connectedUsers);
+  }
+
+  @SubscribeMessage('acceptTradeInit')
+  async acceptTradeInit(client: Socket, payload: any) {
+    const currentUser = this.getUser(client.id, 'socketId');
+    const targetUser = this.getUser(payload.tradingId, 'userId');
+
+    currentUser.user.acceptTrade = true;
+    currentUser.user.trading = {
+      isTrading: true,
+      recipientId: payload.tradingId,
+    };
+
+    this.connectedUsers.splice(currentUser.index, 1, currentUser.user);
+
+    client.to(targetUser.user.socketId).emit('acceptTrade', {
+      message: `${currentUser.user.username} accepts the trade init`,
+    });
+
+    console.log(this.connectedUsers);
+  }
+
+  @SubscribeMessage('sendItem')
+  async sendItem(client: Socket, payload: any) {
+    const { items, tradingId } = payload;
+    const currentUser = this.getUser(client.id, 'socketId');
+    const targetUser = this.getUser(tradingId, 'userId');
+
+    if (
+      currentUser.user.trading.isTrading &&
+      targetUser.user.trading.isTrading
+    ) {
+      currentUser.user.sendingItems = items;
+      targetUser.user.receivingItems = items;
+
+      this.connectedUsers.splice(currentUser.index, 1, currentUser.user);
+      this.connectedUsers.splice(targetUser.index, 1, targetUser.user);
+    }
     console.log(this.connectedUsers);
   }
 }
