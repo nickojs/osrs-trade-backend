cddad4befaa7b709b08bba0235c2b8a900c7ac9d
Nicolau Barsotti <nbarsotti94@gmail.com>
2022-07-03 01:26:51 -0300

    feat: create base for initTrade logic

---
+++ src/socketModel.ts
@@ -0,0 +1,37 @@
+import { APIItem } from './containers/items/items.interface';
+
+export enum SocketEvents {
+  CONNECTION = 'connection',
+  DISCONNECT = 'disconnect',
+  SAVE_USER = 'save_user',
+  SEND_ITEMS = 'send_items',
+  DELIVER_ITEMS = 'deliver_items',
+  OPEN_TRADE = 'open_trade',
+  ACCEPT_TRADE = 'accept_trade',
+  DECLINE_TRADE = 'decline_trade',
+  DECLINE_EXISTING_TRADE = 'decline_existing_trade',
+  FINISH_TRADE = 'finish_trade',
+  SENDER_ACKNOWLEDGE = 'sender_acknowledge',
+  RECIPIENT_ACKNOWLEDGE = 'recipient_acknowledge',
+  REQUEST_USER_STATUS = 'request_user_status',
+  RECIPIENT_STATUS = 'recipient_status',
+  SET_USER_STATUS = 'setUserStatus',
+  ERROR = 'error',
+}
+
+export enum SocketErrors {
+  CONNECTION = "Couldn't establish connection with recipient",
+  TRADE = 'Failed to complete the trade',
+}
+export interface ConnectedUser {
+  socketId: string;
+  userId: string;
+  username: string;
+  trading: {
+    isTrading: boolean;
+    recipientId: string;
+  };
+  acceptTrade: boolean;
+  sendingItems: APIItem[];
+  receivingItems: APIItem[];
+}
--- src/trade.gateway.ts
+++ src/trade.gateway.ts
@@ -2,6 +2,7 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   OnGatewayDisconnect,
+  OnGatewayConnection,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
@@ -14,7 +15,8 @@ type UserStatusBase = typeof STATUS;
 type UserStatus = UserStatusBase[number];
 
 type ConnectedPayload = {
-  id: string;
+  userId: string;
+  username: string;
 };
 
 interface StatusPayload extends ConnectedPayload {
@@ -26,27 +28,52 @@ interface StatusPayload extends ConnectedPayload {
     origin: '*',
   },
 })
-export class TradeGateway implements OnGatewayDisconnect {
+export class TradeGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private dataSource: DataSource) {}
+
+  private connectedUserFactory(
+    userId: string,
+    username: string,
+    socketId: string,
+  ): ConnectedUser {
+    return {
+      userId,
+      socketId,
+      username,
+      trading: {
+        isTrading: false,
+        recipientId: '',
+      },
+      acceptTrade: false,
+      sendingItems: [],
+      receivingItems: [],
+    };
+  }
 
   @WebSocketServer() server: Socket;
   private connectedUsers: Partial<ConnectedUser>[] = [];
+
+  async handleConnection(client: any, ...args: any[]) {
+    console.log(this.connectedUsers);
+  }
 
   @SubscribeMessage('afterConnect')
   setUser(client: Socket, payload: ConnectedPayload) {
     try {
       const findExistingUser = this.connectedUsers.find(
-        (user) => user.userId === payload.id,
+        (user) => user.userId === payload.userId,
       );
       if (findExistingUser) return;
 
-      const createSocketUser = {
-        userId: payload.id,
-        socketId: client.id,
-      };
+      const createSocketUser = this.connectedUserFactory(
+        payload.userId,
+        payload.username,
+        client.id,
+      );
       this.connectedUsers.push(createSocketUser);
+      console.log(this.connectedUsers);
     } catch (error) {
-      client.emit('error', "couldn't update status");
+      client.emit('error', { message: "couldn't update status", error });
     }
   }
 
@@ -55,21 +82,86 @@ export class TradeGateway implements OnGatewayDisconnect {
       (user) => user.socketId === client.id,
     );
     this.connectedUsers.splice(currentUserIndex, 1);
+    console.log(this.connectedUsers);
   }
 
   @SubscribeMessage('setStatus')
   async setUserStatus(client: Socket, payload: StatusPayload) {
     try {
       const userRepo = this.dataSource.getRepository(User);
-      const findUser = await userRepo.findOneBy({ id: payload.id });
+      const findUser = await userRepo.findOneBy({ id: payload.userId });
       if (STATUS.includes(payload.status))
         if (findUser) {
           await userRepo.update(findUser, { status: payload.status });
           client.emit('setStatus', 'updated status');
         }
     } catch (error) {
-      client.emit('error', "couldn't update status");
-      console.log(error);
+      client.emit('error', { message: "couldn't update status", error });
     }
+  }
+
+  @SubscribeMessage('initTrade')
+  async initTrade(client: Socket, payload: any) {
+    const { targetId } = payload;
+    const targetUser = this.connectedUsers.find(
+      (user) => user.userId === targetId,
+    );
+    if (!targetUser) return client.emit('error', { message: 'User is away' });
+
+    const currentUser = this.connectedUsers.find(
+      (user) => user.socketId === client.id,
+    );
+
+    const currentUserIndex = this.connectedUsers.findIndex(
+      (user) => user.socketId === client.id,
+    );
+
+    currentUser.acceptTrade = true;
+    currentUser.trading = {
+      isTrading: true,
+      recipientId: targetId,
+    };
+
+    this.connectedUsers.splice(currentUserIndex, 1, currentUser);
+
+    client.to(targetUser.socketId).emit('initTrade', {
+      message: `${currentUser.username} wants to trade...`,
+      data: {
+        user: {
+          userId: currentUser.userId,
+          socketId: currentUser.socketId,
+        },
+      },
+    });
+
+    console.log(this.connectedUsers);
+  }
+
+  @SubscribeMessage('declineTrade')
+  async declineTrade(client: Socket, payload: any) {
+    const currentUser = this.connectedUsers.find(
+      (user) => user.socketId === client.id,
+    );
+
+    const targetUser = this.connectedUsers.find(
+      (user) => user.userId === payload.tradingId,
+    );
+    const targetUserIndex = this.connectedUsers.findIndex(
+      (user) => user.userId === payload.tradingId,
+    );
+
+    targetUser.acceptTrade = false;
+    targetUser.trading = {
+      isTrading: false,
+      recipientId: '',
+    };
+
+    this.connectedUsers.splice(targetUserIndex, 1, targetUser);
+
+    client.to(targetUser.socketId).emit('declineTrade', {
+      message: `${currentUser.username} declined the trade`,
+    });
+
+    console.log(this.connectedUsers);
   }
 }
